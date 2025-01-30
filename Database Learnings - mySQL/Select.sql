04905177dabe6875f1f9dee60236dd63e034c094
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-30 09:20:27 +0600

    completed select

--- Database Learnings - mySQL/Select.sql
+++ Database Learnings - mySQL/Select.sql
@@ -1,10 +1,39 @@
--- use mydb;
+use mydb;
 
--- select * from employee;
+select * from employee;
 
--- select first_name,last_name
--- from employee;
+select first_name,last_name
+from employee;
 
--- select *
--- from employee
--- where employee_id =1;
+select *
+from employee
+where employee_id =1;
+
+select *
+from employee
+where first_name = "padme";
+
+select *
+from employee
+where hourly_pay>=25.00;
+
+select *
+from employee
+where employee_id != 5;
+
+select *
+from employee
+where hire_date is null;
+
+select *
+from employee
+where hire_date is not null;
+
+
+
+
+
+
+
+
+
