96ada2209f697497d1c664b4bc3d1c5150bb9610
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-30 13:52:22 +0600

    added order by

---
+++ Database Learnings - mySQL/Order By.sql
@@ -0,0 +1,27 @@
+use mydb;
+
+select* from employee
+order by last_name;
+
+select* from employee
+order by last_name desc;
+
+select* from employee
+order by first_name desc;
+
+select* from employee
+order by hire_date;
+
+select* from transaction
+order by amount asc, customer_id desc;
+
+
+
+
+
+
+
+
+
+
+
