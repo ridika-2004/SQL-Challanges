f72b08072abb2173433335140ffe2068d634be81
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-30 13:47:26 +0600

    added wild card characters

---
+++ Database Learnings - mySQL/Wild Card Characters.sql
@@ -0,0 +1,36 @@
+use mydb;
+
+select* from employee
+where first_name like "a%";
+
+select* from employee
+where hire_date like "2025%";
+
+select* from employee
+where first_name like "da%";
+
+select* from employee
+where job like "_ook";
+
+select* from employee
+where hire_date like "____-01-__";
+
+select* from employee
+where hire_date like "____-__-30";
+
+select* from employee
+where job like "_a%";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
