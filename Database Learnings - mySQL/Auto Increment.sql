fb27994a3b4302bb2de5f08b3d9edcf17e31f3f8
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-30 12:20:16 +0600

    added auto increment

---
+++ Database Learnings - mySQL/Auto Increment.sql
@@ -0,0 +1,40 @@
+use mydb;
+
+-- drop table transcations;
+
+-- create table transaction(
+-- transaction_id int primary key auto_increment,
+-- amount decimal(5,2)
+-- );
+
+-- select * from transaction;
+
+-- insert into transaction (amount)
+-- values (4.99);
+
+-- select * from transaction;
+
+-- insert into transaction (amount)
+-- values (2.89), (3.88), (4.6);
+
+-- select * from transaction;
+
+-- alter table transaction
+-- auto_increment = 1000;
+
+-- delete from transaction;
+
+-- select * from transaction;
+
+insert into transaction (amount)
+values (2.89), (3.88), (4.6);
+
+select * from transaction;
+
+
+
+
+
+
+
+
