c31f0e19b7781b3d0359617c2728bede854c2368
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-07-18 19:59:46 +0600

    Updated anonymous block.sql

--- PL-SQL/anonymous block.sql
+++ PL-SQL/anonymous block.sql
@@ -2,10 +2,11 @@
 set SERVEROUTPUT on;
 
 declare
-num1 number(10) :=5;
+num1 constant number(10) :=5; -- cannot be overridden
 num2 number(10) :=10;
 res number(10);
 begin
+-- num1 :=30 will give error because num1 is constant
 res :=num1+num2;
 dbms_output.put_line('The sum of ' || num1 || ' and ' || num2 || ' is ' || res);
 end;
@@ -19,3 +20,11 @@ begin
 res :=num1+num2;
 dbms_output.put_line('The sum of ' || num1 || ' and ' || num2 || ' is ' || res);
 end;
+
+-- select statement
+select 'Hi this is ridika' from dual;
+
+
+
+
+
