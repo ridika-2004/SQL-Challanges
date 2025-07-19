0b38329b1bd8e2d54b6e71c1a4b22fac6d7b7040
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-07-19 23:51:22 +0600

    Updated Procedure.sql

--- PL-SQL/Procedure.sql
+++ PL-SQL/Procedure.sql
@@ -28,4 +28,37 @@ exec square(6, :x);
 
 print x;
 
+-- procedure for updating a column of a table
+select * from book;
+
+create or replace procedure price_reduce(title_name in varchar2) is
+v_price number;
+begin
+select price into v_price
+from book
+where title = title_name;
+dbms_output.put_line('previous price of '|| title_name || ' is ' || v_price);
+update book
+set price = price*0.5
+where title = title_name;
+select price into v_price
+from book
+where title = title_name;
+dbms_output.put_line('current price of '|| title_name || ' is ' || v_price);
+end;
+
+exec price_reduce('1984');
+
+--insert through procedure
+
+create or replace procedure book_insert(title1 in varchar2, author1 in varchar2, genre1 in varchar2, price1 in number, published_on1 in varchar2, available1 in char) is
+begin
+insert into book(title, author, genre, price, published_on, available) values
+(title1, author1, genre1, price1, to_date(published_on1, 'yyyy-mm-dd'), available1);
+end;
+
+exec book_insert('To Kill a Mockingbird', 'Harper Lee', 'Classic Fiction', 12.99, '1960-07-11', 'Y');
+
+
+
 drop procedure square;
